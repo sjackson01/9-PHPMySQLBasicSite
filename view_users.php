bd94ddef16091d1dd5bb6299394bd15b719c7699
Steven Jackson <stevenpjackson0@gmail.com>
2020-02-17 22:34:35 -0600

    Fix (view_users.php > Hunk 1) : Fix Query and delete/ edit links.

--- view_users.php
+++ view_users.php
@@ -9,9 +9,15 @@ echo '<h1>Registered Users</h1>';
 
 require('mysqli_connect.php'); // Connect to the db.
 
-// Make the query:
-$q = "SELECT CONCAT(last_name, ', ', first_name) AS name, DATE_FORMAT(registration_date, '%M %d, %Y') AS dr FROM users ORDER BY registration_date ASC";
-$r = @mysqli_query($dbc, $q); // Run the query.
+//Make
+$q = "SELECT last_name, first_name, 
+	  DATE_FORMAT(registration_date, '%M %d, %Y') 
+	  AS dr, user_id 
+	  FROM users 
+	  ORDER BY registration_date 
+	  ASC";
+
+$r = @mysqli_query($dbc, $q);
 
 // Count the number of returned rows:
 $num = mysqli_num_rows($r);
@@ -22,19 +28,43 @@ if ($num > 0) { // If it ran OK, display the records.
 	echo "<p>There are currently $num registered users.</p>\n";
 
 	// Table header.
+	
 	echo '<table width="60%">
 	<thead>
-	<tr>
-		<th align="left">Name</th>
-		<th align="left">Date Registered</th>
-	</tr>
+		<tr>
+		<th align="left"><strong>Edit
+		</strong></th>
+		<th align="left"><strong>
+		Delete</strong></th>
+		<th align="left"><strong>
+		Last Name</strong></th>
+		<th align="left"><strong>
+		First Name</strong></th>
+		<th align="left"><strong>
+		Date Registered</strong></th>
+		</tr>
 	</thead>
 	<tbody>
-';
+	';
 
 	// Fetch and print all the records:
 	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-		echo '<tr><td align="left">' . $row['name'] . '</td><td align="left">' . $row['dr'] . '</td></tr>
+		echo '<tr>
+			<td align="left">
+			<a href="edit_user.php?id=' .
+			$row['user_id'] . '">Edit</a>
+			</td>
+			<td align="left">
+			<a href="delete_user.php?id='
+			. $row['user_id'] . '">Delete
+			</a></td>
+			<td align="left">' .
+			$row['last_name'] . '</td>
+			<td align="left">' .
+			$row['first_name'] . '</td>
+			<td align="left">' . $row['dr']
+			. '</td>
+			</tr>
 		';
 	}
 
