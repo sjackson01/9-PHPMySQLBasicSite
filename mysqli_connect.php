cecd489308d57067492a2284321fdd13219dab69
sjackson02 <jackson7@lsu.edu>
2020-02-24 08:05:18 -0600

    Modify (mysqli_connect.php>Hunk 1) Edit  password and host name for connection within docker enviroment. Add (.vscode/launch.json>Hunk 1) Create .json file for xdbug.

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -6,8 +6,8 @@
 
 // Set the database access information as constants:
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost:3306');
+define('DB_PASSWORD', 'tiger');
+define('DB_HOST', 'mysql:3306');
 define('DB_NAME', 'sitename');
 
 
